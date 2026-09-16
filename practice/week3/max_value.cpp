c2a7e2821c14d52121b611339249ced69520bd87
Min <houeges@icloud.com>
2026-09-16 15:51:13 +0900

    week3-2. 조건문 실습

---
+++ practice/week3/max_value.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    char secreat_code = 'h';
+    cout << "비밀코드를 맞춰보세요: ";
+    char code;
+    cin >> code;
+
+    if (code < secreat_code) {
+        cout << code << "뒤에 있음" << endl;
+    }
+    else if (code > secreat_code) {
+        cout << code << "앞에 있음" << endl;
+    }
+    else {
+        cout << "맞추었습니다." << endl;
+    }
+    return 0;
+}
